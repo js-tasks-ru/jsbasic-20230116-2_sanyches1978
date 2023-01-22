c38ef7ba62efbdf65d1bd91e3ad9936e90dd2327
Alexandr Ananev <sanyches1978@gmail.com>
2023-01-22 18:48:07 +0100

    added function sumSalary(salaries)

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,16 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  
+  for( let key in salaries) {
+    let elem = salaries[key];
+
+    if( typeof(elem) === 'number' 
+      && !isNaN(elem)
+      && elem !== Infinity
+      && elem !== -Infinity
+      ) {
+      sum += elem;
+    }
+  }
+  return sum;   // ваш код...
 }
