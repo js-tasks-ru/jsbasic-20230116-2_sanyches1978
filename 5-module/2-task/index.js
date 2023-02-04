29dd3b01e8f9f5ef7b081b126563c89ef962f2a6
Alexandr Ananev <sanyches1978@gmail.com>
2023-02-04 20:44:52 +0100

    fix: changed function toggle

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -2,16 +2,17 @@ function toggleText() {
   let button = document.querySelector('.toggle-text-button');
   let elem = document.querySelector('#text');
  
-  function hide() {
+  /*function hide() {
     elem.hidden = true;
   }
 
   function show() {
     elem.hidden = false;
-  }
+  }*/
 
   function toggle() {
-    elem.hidden == false ? hide() : show();
+    //elem.hidden == false ? hide() : show();
+    elem.hidden = !elem.hidden
   }
 
   button.addEventListener('click', toggle);
